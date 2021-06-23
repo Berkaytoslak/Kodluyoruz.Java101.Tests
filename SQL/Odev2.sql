59991361de918548d062f0fcadaeabcb357af437
Berkay Toslak <berkaytslk15@gmail.com>
2021-06-24 00:13:07 +0300

    Odev2 SQL

---
+++ SQL/Odev2.sql
@@ -0,0 +1,5 @@
+SELECT * FROM public.film AS F WHERE F.replacement_cost BETWEEN 12.99 AND 16.99;
+
+SELECT first_name,last_name FROM public.actor WHERE first_name IN ('Penelope','Nick','Ed');
+
+SELECT * FROM public.film WHERE rental_rate IN (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99);
